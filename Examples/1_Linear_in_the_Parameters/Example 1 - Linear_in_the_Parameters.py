ff9068cf2fbf44316c4a0a290f674118528227fe
Sté Thunus <144615547+Stee-T@users.noreply.github.com>
2025-08-31 19:21:34 +0200

    Examples: Strongly type Example 1

--- Examples/1_Linear_in_the_Parameters/Example 1 - Linear_in_the_Parameters.py
+++ Examples/1_Linear_in_the_Parameters/Example 1 - Linear_in_the_Parameters.py
@@ -1,5 +1,5 @@
 # ---------------------------------------------------- 1. Imports
-
+from typing import Any, Optional
 import torch as tor
 
 import matplotlib.pyplot as plt
@@ -9,13 +9,13 @@ import NARMAX
 import NARMAX.Test as Test_Systems
 
 # ---------------------------------------------------- 2. Hyper-parameters
-p = 2_500 # Dataset size
-InputAmplitude = 1
-tol = 0.0001 # Fitting tolerance in %, so multiply by 100
-W = None # Noise
-qx = 4; qy = 4 # maximum x and y delays
-ExpansionOrder = 3 # monomial expansion order
-ArboDepth = 4 # maximum number of levels the arborescence can have
+p: int = 2_500 # Dataset size
+InputAmplitude: float = 1.0
+tol: float = 0.0001 # Fitting tolerance in %, so multiply by 100
+W: Optional[tor.Tensor] = None # Noise
+qx: int = 4; qy: int = 4 # maximum x and y delays
+ExpansionOrder: int = 3 # monomial expansion order
+ArboDepth: int = 4 # maximum number of levels the arborescence can have
 
 # System choice
 # Sys = Test_Systems.ARX
@@ -26,13 +26,13 @@ Sys = Test_Systems.NonLinearities
 
 # Generate x and y data
 while ( 5 ): # 5 is the absolute truth, do while y contains no nan
-  x = ( InputAmplitude * 2 ) * ( tor.rand( p, device = NARMAX.device ) - 0.5 ) # uniformly distributed white noise
+  x: tor.Tensor = ( InputAmplitude * 2 ) * ( tor.rand( p, device = NARMAX.device ) - 0.5 ) # uniformly distributed white noise
   x -= tor.mean( x ) # center: VERY IMPORTANT!
   x, y, W = Sys( x, W, Print = True ) # apply selected system
   if ( not tor.isnan( tor.sum( y ) ) ): break
 
 
-NonLinearities = [ NARMAX.Identity ] # List of NonLinearity objects, must start with identity
+NonLinearities: list[ NARMAX.NonLinearity ] = [ NARMAX.Identity ] # List of NonLinearity objects, must start with identity
 NonLinearities.append( NARMAX.NonLinearity( "abs", f = tor.abs ) )
 NonLinearities.append( NARMAX.NonLinearity( "cos", f = tor.cos ) )
 NonLinearities.append( NARMAX.NonLinearity( "exp", f = tor.exp ) )
@@ -44,7 +44,7 @@ RegMat, RegNames, _ = NARMAX.CTors.NonLinearizer( y, RegMat, RegNames, NonLinear
 
 
 # ---------------------------------------------------- 4. Validation Data
-ValidationDict = { # contains essentially everything passed to the CTors to reconstruct the regressors
+ValidationDict: dict[ str, Any ] = { # contains essentially everything passed to the CTors to reconstruct the regressors
   "y": [],
   "Data": [],
   "InputVarNames": [ "x", "y" ], # variables in Data, etc
@@ -53,7 +53,7 @@ ValidationDict = { # contains essentially everything passed to the CTors to reco
 
 for i in range( 5 ): # Fill the validation dict's data entry with randomly generated validation data
   while ( 5 ): # 5 is the absolute truth
-    x_val = tor.rand( int( p / 3 ) ) # Store since used twice
+    x_val: tor.Tensor = tor.rand( int( p / 3 ) ) # Store since used twice
     x_val -= tor.mean( x_val ) # center
     x_val, y_val, W = Sys( x_val, W, Print = False ) # _val to avoid overwriting the training y
     if ( not tor.isnan( tor.sum( y_val ) ) ): break # Remain in the loop until no NaN
@@ -66,15 +66,15 @@ for i in range( 5 ): # Fill the validation dict's data entry with randomly gener
 # File = "Some/Valid/Path/FileName.pkl"
 
 Arbo = NARMAX.Arborescence( y,
-                             Ds = None, DsNames = None, # Ds & Regressor names, being dictionary of selected regressors
-                             Dc = RegMat, DcNames = RegNames, # Dc & Regressor names, being dictionnary of candidate regerssors (phi)
-                             tolRoot = tol, tolRest = tol, # \rho tolerances
-                             MaxDepth = ArboDepth, # Maximal number of levels
-                             ValFunc = NARMAX.DefaultValidation, ValData = ValidationDict, # Validation function and dictionary
-                             Verbose = False, # Print the current state of the FOrLSR (only meaningful for regressions with many terms)
-                             # FileName = File, # Path and File to save the Backups into
-                             # SaveFrequency = 10, # Save frequency in minutes
-                            )
+                            Ds = None, DsNames = None, # Ds & Regressor names, being dictionary of selected regressors
+                            Dc = RegMat, DcNames = RegNames, # Dc & Regressor names, being dictionnary of candidate regerssors (phi)
+                            tolRoot = tol, tolRest = tol, # \rho tolerances
+                            MaxDepth = ArboDepth, # Maximal number of levels
+                            ValFunc = NARMAX.DefaultValidation, ValData = ValidationDict, # Validation function and dictionary
+                            Verbose = False, # Print the current state of the FOrLSR (only meaningful for regressions with many terms)
+                            # FileName = File, # Path and File to save the Backups into
+                            # SaveFrequency = 10, # Save frequency in minutes
+                          )
 
 Arbo.fit()
 
@@ -86,6 +86,6 @@ Arbo.fit()
 Figs, Axs = Arbo.PlotAndPrint( ValidationDict ) # returns both figures and axes for further processing, as as the zoom-in below
 Axs[0][0].set_xlim( [0, 500] ) # Force a zoom-in
 
-theta, L, ERR, _, RegMat, RegNames = Arbo.get_Results() # See Tutorial 2 for how to create a NARMX object.
+theta, L, ERR, _, RegMat, RegNames = Arbo.get_Results() # See Tutorial 2 for how use that data!
 
 plt.show()
